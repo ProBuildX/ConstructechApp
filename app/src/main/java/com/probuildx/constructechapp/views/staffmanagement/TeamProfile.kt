fc390bdfe3765d86a61f533cdbb4d9bf2d74167d
jufer10 <fcarrasco023@gmail.com>
2024-11-19 23:34:01 -0500

    feat: add TeamProfile Workers

--- app/src/main/java/com/probuildx/constructechapp/views/staffmanagement/TeamProfile.kt
+++ app/src/main/java/com/probuildx/constructechapp/views/staffmanagement/TeamProfile.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -19,29 +20,50 @@ import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import com.probuildx.constructechapp.entities.Team
+import com.probuildx.constructechapp.entities.Worker
 import com.probuildx.constructechapp.viewmodels.TeamsViewModel
+import com.probuildx.constructechapp.viewmodels.WorkersViewModel
 
 @Composable
-fun TeamProfileScreen(navController: NavController, teamId: Int, teamsVm: TeamsViewModel = viewModel()) {
+fun TeamProfileScreen(
+    navController: NavController,
+    teamId: Int,
+    teamsVm: TeamsViewModel = viewModel(),
+    workerVm: WorkersViewModel = viewModel()
+) {
     val team by teamsVm.team.collectAsState()
-    val isLoading by teamsVm.isLoading.collectAsState()
-    val errorMessage by teamsVm.errorMessage.collectAsState()
+    val isLoadingT by teamsVm.isLoading.collectAsState()
+    val errorMessageT by teamsVm.errorMessage.collectAsState()
 
-    LaunchedEffect(teamId) { teamsVm.getById(teamId) }
+    val workers by workerVm.workers.collectAsState()
+    val isLoadingW by teamsVm.isLoading.collectAsState()
+    val errorMessageW by teamsVm.errorMessage.collectAsState()
+
+    LaunchedEffect(teamId) {
+        teamsVm.getById(teamId)
+        workerVm.getByTeam(teamId)
+    }
 
     when {
-        (isLoading || team == null) -> CircularProgressIndicator(modifier = Modifier.fillMaxSize())
-        errorMessage != null -> Text("$errorMessage", modifier = Modifier.fillMaxSize())
+        (isLoadingT || isLoadingW || team == null) ->
+            CircularProgressIndicator(modifier = Modifier.fillMaxSize())
+        errorMessageW != null -> Text("$errorMessageW", modifier = Modifier.fillMaxSize())
+        errorMessageT != null -> Text("$errorMessageT", modifier = Modifier.fillMaxSize())
         else -> {
 
-            TeamProfile(navController, team!!)
+            TeamProfile(navController, team!!, workers)
 
         }
     }
 }
 
 @Composable
-fun TeamProfile(navController: NavController, team: Team, teamsVm: TeamsViewModel = viewModel()) {
+fun TeamProfile(
+    navController: NavController,
+    team: Team,
+    workers: List<Worker>,
+    teamsVm: TeamsViewModel = viewModel()
+) {
     //TODO: mejorar interfaz
     Column(
         modifier = Modifier
@@ -49,7 +71,12 @@ fun TeamProfile(navController: NavController, team: Team, teamsVm: TeamsViewMode
             .fillMaxWidth(),
         verticalArrangement = Arrangement.spacedBy(8.dp)
     ) {
-        Text(text = "Name: ${team.name} ")
+        Text("Name: ${team.name} ")
+
+        Text("Workers:")
+        workers.forEach { worker ->
+            Text("${worker.name} ${worker.lastName}")
+        }
 
         Button(
             onClick = {
